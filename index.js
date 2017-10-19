a8ea721df423da17c5e05d445c958f50bd3c51b8
unknown <you@example.com>
2017-10-19 17:29:33 -0500

    mi primer commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const lodash=require('lodash')
+console.log('hola mundo')
+
+const obj ={
+  nombre: 'juan',
+  edad: 18,
+  email:'tlaxcalajuanQgmail.com',
+}
+
+function hola() {
+  return 'hola mundo x2'
+}
+
+console.log(obj.email)
+
+//console.log(hola())
+//console.log(2+2)
+
+//var lista=[1,2,3,4,5]
+
+//console.log(lista)
